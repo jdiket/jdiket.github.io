686756069b3c997d116ae77ea804f6c9e6626b98
John <john.diket@gmail.com>
2021-05-08 09:17:56 -0400

    questions file and format created

---
+++ questions.js
@@ -0,0 +1,195 @@
+const movies = {
+    100: {
+        value: 100,
+        question: 'This was the first movie to gross over $1 billion at the Box Office.',
+        answers: ['Titanic', 'Jurassic Park', 'Star Wars: The Phantom Menace', 'The Lord of the Rings: Return of the King'],
+    }, 
+    200: {
+        value: 200,
+        question: 'Due to his dramatic weight gain to play the role, this Batman actor was called "Fatman" by many of the set.',
+        answers: ['Christian Bale', 'Michael Keaton', 'Adam West', 'George Clooney']
+    },
+    300: {
+        value: 300,
+        question: 'This film out of South Korea became the first Oscar winner for Best Picture.',
+        answers: ['Parasite', 'A Taxi Driver', 'The Age of Shadows', 'Burning']
+    },
+    400: {
+        value: 400,
+        question: 'Famed and award winning actor Paul Newman made his final theatrical appearance in this Same Mendes film.',
+        answers: ['Road to Perdition', 'American Beauty', 'Twilight', 'Where the Money Is']
+    },
+    500: {
+        value: 500,
+        question: 'The PG-13 film rating was created in response to outrage over scenes depicted in this 80s film.',
+        answers: ['Indiana Jones and the Temple of Doom', 'Red Dawn', 'Escape From New York', 'Gremlins']
+    }
+};
+
+const femaleAuthors = {
+    100: {
+        value: 100,
+        question: 'Famously rejected by nearly half a dozen publishing houses, this author went on to become the wealthiest woman in English.',
+        answers: ['J.K. Rowling', 'Virginia Woolf', 'Agatha Christie', 'P.D. James']
+    }, 
+    200: {
+        value: 200,
+        question: 'In 1988 she scored her first #1 bestseller with "Queen of the Damned", the third novel of her Vampire Chronicles.',
+        answers: ['Anne Rice', 'Laurell K. Hamilton', 'Nancy Collins', 'Nancy Baker']
+    }, 
+    300: {
+        value: 300,
+        question: '',
+        answers: ['', '', '', '']
+    }, 
+    400: {
+        value: 400,
+        question: '',
+        answers: ['', '', '', '']
+    }, 
+    500: {
+        value: 500,
+        question: '',
+        answers: ['', '', '', '']
+    }
+};
+
+const category = {
+    100: {
+        value: 100,
+        question: '',
+        answers: ['', '', '', '']
+    }, 
+    200: {
+        value: 200,
+        question: '',
+        answers: ['', '', '', '']
+    }, 
+    300: {
+        value: 300,
+        question: '',
+        answers: ['', '', '', '']
+    }, 
+    400: {
+        value: 400,
+        question: '',
+        answers: ['', '', '', '']
+    }, 
+    500: {
+        value: 500,
+        question: '',
+        answers: ['', '', '', '']
+    }
+};
+
+const category = {
+    100: {
+        value: 100,
+        question: '',
+        answers: ['', '', '', '']
+    }, 
+    200: {
+        value: 200,
+        question: '',
+        answers: ['', '', '', '']
+    }, 
+    300: {
+        value: 300,
+        question: '',
+        answers: ['', '', '', '']
+    }, 
+    400: {
+        value: 400,
+        question: '',
+        answers: ['', '', '', '']
+    }, 
+    500: {
+        value: 500,
+        question: '',
+        answers: ['', '', '', '']
+    }
+};
+
+const category = {
+    100: {
+        value: 100,
+        question: '',
+        answers: ['', '', '', '']
+    }, 
+    200: {
+        value: 200,
+        question: '',
+        answers: ['', '', '', '']
+    }, 
+    300: {
+        value: 300,
+        question: '',
+        answers: ['', '', '', '']
+    }, 
+    400: {
+        value: 400,
+        question: '',
+        answers: ['', '', '', '']
+    }, 
+    500: {
+        value: 500,
+        question: '',
+        answers: ['', '', '', '']
+    }
+};
+
+const category = {
+    100: {
+        value: 100,
+        question: '',
+        answers: ['', '', '', '']
+    }, 
+    200: {
+        value: 200,
+        question: '',
+        answers: ['', '', '', '']
+    }, 
+    300: {
+        value: 300,
+        question: '',
+        answers: ['', '', '', '']
+    }, 
+    400: {
+        value: 400,
+        question: '',
+        answers: ['', '', '', '']
+    }, 
+    500: {
+        value: 500,
+        question: '',
+        answers: ['', '', '', '']
+    }
+};
+
+const category = {
+    100: {
+        value: 100,
+        question: '',
+        answers: ['', '', '', '']
+    }, 
+    200: {
+        value: 200,
+        question: '',
+        answers: ['', '', '', '']
+    }, 
+    300: {
+        value: 300,
+        question: '',
+        answers: ['', '', '', '']
+    }, 
+    400: {
+        value: 400,
+        question: '',
+        answers: ['', '', '', '']
+    }, 
+    500: {
+        value: 500,
+        question: '',
+        answers: ['', '', '', '']
+    }
+};

1bd2f05ede6e8227b7f5d5922db91b311157e595
John <john.diket@gmail.com>
2021-05-10 21:51:38 -0400

    questions can be answered in a row and score is tallied for correct responses

--- appTest.js
+++ appTest.js
@@ -1,5 +1,7 @@
+// Establishing global variable for play
 let playerScore = 0;
 
+// Creating variables to use for event listeners and choices
 const qCard = document.querySelector('#qCard');
 const qText = document.querySelector('#qText');
 const choice1 = document.querySelector('#choice_1');
@@ -13,31 +15,41 @@ const testing = {
     three: ['test1', 'test2', 'test3', 'test4']
 };
 
+
+/* This function displays the modal card with question and answer choices when a category and value has been selected.  It turns on the modal view, then injects the text of the question and builds a temporary array for the answers so they can be randomized while maintaining original syntax. */
 const showQuestion = () => {
     qCard.style.display = 'block';
     qText.innerText = testing.two;
     let randAnswerArray = [];
-    let tempArray = testing.three;
+    let tempArray = [testing.three[0], testing.three[1], testing.three[2], testing.three[3]];
     for (let i = 0; i < 4; i ++) {
         let randChoice = Math.floor(Math.random() * tempArray.length);
         randAnswerArray.push(tempArray[randChoice]);
         tempArray.splice(randChoice, 1);
-        console.log(randChoice);
-        console.log(tempArray);
-        console.log(randAnswerArray);
     }
     choice1.innerText = randAnswerArray[0];
     choice2.innerText = randAnswerArray[1];
     choice3.innerText = randAnswerArray[2];
     choice4.innerText = randAnswerArray[3];
-}
-
-const answerQuestion = (e) => {
-    console.log(e.currentTarget.innerText);
-    qCard.style.display = 'none';
+    console.log(testing.three)
     
 }
 
+console.log(testing.three[0])
+
+/* Turns the modal card back off to redisplay the game board */
+const answerQuestion = (e) => {
+    console.log(e.currentTarget.innerText);
+    qCard.style.display = 'none';
+    console.log(testing.three[0])
+    if (e.currentTarget.innerText === testing.three[0]) {
+        playerScore += testing.one
+    }
+    console.log(playerScore)
+    
+}
+/* Creates an event listener on all question spaces of the board that will activate the showQuestion funciton */
 document.querySelectorAll('.qButtons').forEach(question => question.addEventListener('click', showQuestion));
 
+/* Creates event listener on the answer choices for answering the questions */
 document.querySelectorAll('.aButtons').forEach(playerAnswer => playerAnswer.addEventListener('click', answerQuestion));

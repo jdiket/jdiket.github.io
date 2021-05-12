9cf2a8eb828e5e14c60c5440aa14bf80e45dcffc
John <john.diket@gmail.com>
2021-05-11 21:37:18 -0400

    added functionality to prevent re=answering question multiple times

--- README.md
+++ README.md
@@ -3,3 +3,9 @@
 ## Overview
 
 For Project 1, I decided to create Jeopardy.  While at its surface a very simple game, this project has several hurdles to get over in order to reach a completed product.  Upon opening the webpage, the player is greeted by the game title, a score, and the instruction to select a category and value.  Selecting any value of a category will open a question card with multiple choice options.  Once the player has selected an answer, they are notified if right or wrong, the score is updated, and they are returned to the game board.  By default the game is set to record how many point the player can score in 5 minutes, with wrong answers being worth negative points.
+
+### Technologies Used:
+- Languages - HTML5, CSS3, Javascript
+- Design - Google Fonts
+
+
--- app.js
+++ app.js
@@ -21,26 +21,31 @@ const qAndA = {
             value: 100,
             question: 'This was the first movie to gross over $1 billion at the Box Office.',
             answers: ['Titanic', 'Jurassic Park', 'Star Wars: The Phantom Menace', 'The Lord of the Rings: Return of the King'],
+            answered: false,
         }, 
         200: {
             value: 200,
             question: 'Due to his dramatic weight gain to play the role, this Batman actor was called "Fatman" by many on the set.',
-            answers: ['Christian Bale', 'Michael Keaton', 'Adam West', 'George Clooney']
+            answers: ['Christian Bale', 'Michael Keaton', 'Adam West', 'George Clooney'],
+            answered: false,
         },
         300: {
             value: 300,
             question: 'This film out of South Korea became the first foreign language Oscar winner for Best Picture.',
-            answers: ['Parasite', 'A Taxi Driver', 'The Age of Shadows', 'Burning']
+            answers: ['Parasite', 'A Taxi Driver', 'The Age of Shadows', 'Burning'],
+            answered: false,
         },
         400: {
             value: 400,
             question: 'Famed and award winning actor Paul Newman made his final theatrical appearance in this Sam Mendes film.',
-            answers: ['Road to Perdition', 'American Beauty', 'Twilight', 'Where the Money Is']
+            answers: ['Road to Perdition', 'American Beauty', 'Twilight', 'Where the Money Is'],
+            answered: false,
         },
         500: {
             value: 500,
             question: 'The PG-13 film rating was created in response to outrage over scenes depicted in this 80s film.',
-            answers: ['Indiana Jones and the Temple of Doom', 'Red Dawn', 'Escape From New York', 'Gremlins']
+            answers: ['Indiana Jones and the Temple of Doom', 'Red Dawn', 'Escape From New York', 'Gremlins'],
+            answered: false,
         }
     },
 
@@ -48,27 +53,32 @@ const qAndA = {
         100: {
             value: 100,
             question: 'Famously rejected by nearly half a dozen publishing houses, this author went on to become the wealthiest woman in England.',
-            answers: ['J.K. Rowling', 'Virginia Woolf', 'Agatha Christie', 'P.D. James']
+            answers: ['J.K. Rowling', 'Virginia Woolf', 'Agatha Christie', 'P.D. James'],
+            answered: false,
         }, 
         200: {
             value: 200,
             question: 'In 1988, she scored her first #1 bestseller with "Queen of the Damned", the third novel of her Vampire Chronicles.',
-            answers: ['Anne Rice', 'Laurell K. Hamilton', 'Nancy Collins', 'Nancy Baker']
+            answers: ['Anne Rice', 'Laurell K. Hamilton', 'Nancy Collins', 'Nancy Baker'],
+            answered: false,
         }, 
         300: {
             value: 300,
             question: 'Like Michael Chrichton, Arthur Conan Doyle wrote a thriller with this title about a land inhabited by dinosaurs.',
-            answers: ['The Lost World', 'Land of the Lost', 'Jurassic Park', 'Prey']
+            answers: ['The Lost World', 'Land of the Lost', 'Jurassic Park', 'Prey'],
+            answered: false,
         }, 
         400: {
             value: 400,
             question: '"The Last of the Mohicans" takes place against the backdrop of this 18th century war.',
-            answers: ['The French and Indian War', 'The War of 1812', 'Seven Years\'s War', 'The American Revolution']
+            answers: ['The French and Indian War', 'The War of 1812', 'Seven Years\'s War', 'The American Revolution'],
+            answered: false,
         }, 
         500: {
             value: 500,
             question: 'Brian Garfield\'s wife had her purse stolen, so Brian wrote this novel about a vigilante whose wife has been murdered.',
-            answers: ['Death Wish', 'The Fugitive', 'My Wife\'s Murder', 'Gone Girl']
+            answers: ['Death Wish', 'The Fugitive', 'My Wife\'s Murder', 'Gone Girl'],
+            answered: false,
         }
     },
 
@@ -76,27 +86,32 @@ const qAndA = {
         100: {
             value: 100,
             question: 'While the most abundant element in the universe, this element does not appear naturally on Earth.',
-            answers: ['Hydrogen', 'Helium', 'Lithium', 'Berylium']
+            answers: ['Hydrogen', 'Helium', 'Lithium', 'Berylium'],
+            answered: false,
         }, 
         200: {
             value: 200,
             question: 'The father of the modern periodic table, he famously designed it based on a love of Solitaire.',
-            answers: ['Mendeleev', 'Moseley', 'Heisenberg', 'Plank']
+            answers: ['Mendeleev', 'Moseley', 'Heisenberg', 'Plank'],
+            answered: false,
         }, 
         300: {
             value: 300,
             question: 'The creator of the Periodic Table incorrectly stated this family of elements did not exist.',
-            answers: ['The Noble Gases', 'Alkali Metals', 'Halogens', 'Silicons']
+            answers: ['The Noble Gases', 'Alkali Metals', 'Halogens', 'Silicons'],
+            answered: false,
         }, 
         400: {
             value: 400,
             question: 'This is the term for the various forms of a single element.',
-            answers: ['Allotrope', 'Compound', 'Ion', 'Molecule']
+            answers: ['Allotrope', 'Compound', 'Ion', 'Molecule'],
+            answered: false,
         }, 
         500: {
             value: 500,
             question: 'This atomic number was theorized by Richard Feynman to be the theoretical limit of the periodic table.',
-            answers: ['137', '124', '142', '121']
+            answers: ['137', '124', '142', '121'],
+            answered: false,
         }
     },
 
@@ -104,27 +119,32 @@ const qAndA = {
         100: {
             value: 100,
             question: 'A carpenter, Ole Kirk Christiansen, founded Lego in the 1930s to make wooden toys; he coined the company name from the words "leg godt", "play well" in this, his native language',
-            answers: ['Danish', 'Dutch', 'German', 'Finnish']
+            answers: ['Danish', 'Dutch', 'German', 'Finnish'],
+            answered: false,
         }, 
         200: {
             value: 200,
             question: 'Popular sets from the building series include such landmarks as Big Ben, the Eiffel Tower, and this World Heritage Site and masterpiece of Indian culture.',
-            answers: ['The Taj Mahal', 'Angkar Wot', 'Dilwara Temple', 'The Sun Temple']
+            answers: ['The Taj Mahal', 'Angkar Wot', 'Dilwara Temple', 'The Sun Temple'],
+            answered: false,
         }, 
         300: {
             value: 300,
             question: 'In 2016 NASA\'s Juno probe completed its 5-year voyage to Jupiter; on board is a minifigure of this 17th century Italian, telescope in hand.',
-            answers: ['Galileo', 'Schiparelli', 'Cassini', 'Giaconni']
+            answers: ['Galileo', 'Schiparelli', 'Cassini', 'Giaconni'],
+            answered: false,
         }, 
         400: {
             value: 400,
             question: 'In 2015, Lego Group inaugurated its own off-shore wind farm & every employee was given a model wind farm kit, symbolizing the company\'s goal of being 100% based on this type of energy by 2020.',
-            answers: ['renewable', 'wind', 'solar', 'green']
+            answers: ['renewable', 'wind', 'solar', 'green'],
+            answered: false,
         }, 
         500: {
             value: 500,
             question: 'The Mindstorms construction kit, which allows children to build & program their own robotic devices, grew out of the company\'s longstanding partnership with the famous Media Lab of this Cambridge, Massachusetts school.',
-            answers: ['MIT', 'Harvard', 'Boston University', 'Carnegie Mellon']
+            answers: ['MIT', 'Harvard', 'Boston University', 'Carnegie Mellon'],
+            answered: false,
         }
     },
 
@@ -132,27 +152,32 @@ const qAndA = {
         100: {
             value: 100,
             question: 'Due to lawsuits, in the 1940s Pinesol had to change its name to Pine-sol to avoid confusion with this other cleaning brand.',
-            answers: ['Lysol', 'Pine Fresh', 'Cleansol', 'Clorox']
+            answers: ['Lysol', 'Pine Fresh', 'Cleansol', 'Clorox'],
+            answered: false,
         }, 
         200: {
             value: 200,
             question: 'Crepe paper filters used in gas masks during World War 1 evolved into this brand of tissue.',
-            answers: ['Kleenex', 'Puffs', 'Charmin', 'Cottonelle']
+            answers: ['Kleenex', 'Puffs', 'Charmin', 'Cottonelle'],
+            answered: false,
         }, 
         300: {
             value: 300,
             question: 'Launched nationally in 1953, this wrap that clings to leftovers was Dow Chemical\'s first consumer product.',
-            answers: ['Saran Wrap', 'Cling Wrap', 'Plastic Wrap', 'Reynolds Wrap']
+            answers: ['Saran Wrap', 'Cling Wrap', 'Plastic Wrap', 'Reynolds Wrap'],
+            answered: false,
         }, 
         400: {
             value: 400,
             question: 'In 1958 this credit card began as BankAmericard when cards with a $500 limit were sent out as an experiment.',
-            answers: ['Visa', 'American Express', 'MasterCard', 'Discover']
+            answers: ['Visa', 'American Express', 'MasterCard', 'Discover'],
+            answered: false,
         }, 
         500: {
             value: 500,
             question: 'The strawberry flavor of this 5-letter Coca-Cola brand is a favorite spirit offering at shrines in Thailand.',
-            answers: ['Fanta', 'Barq\'s', 'Costa', 'Chico']
+            answers: ['Fanta', 'Barq\'s', 'Costa', 'Chico'],
+            answered: false,
         }
     },
 
@@ -160,27 +185,32 @@ const qAndA = {
         100: {
             value: 100,
             question: 'On this planet named for a king amongst gods, the largest circular storm in our solar system takes place.',
-            answers: ['Jupiter', 'Saturn', 'Neptune', 'Venus']
+            answers: ['Jupiter', 'Saturn', 'Neptune', 'Venus'],
+            answered: false,
         }, 
         200: {
             value: 200,
             question: 'This is the largest asteroid known to exist.',
-            answers: ['Ceres', 'Vesta', 'Icarus', 'Eros']
+            answers: ['Ceres', 'Vesta', 'Icarus', 'Eros'],
+            answered: false,
         }, 
         300: {
             value: 300,
             question: 'This was the first spacecraft to life a human beign into space.',
-            answers: ['Vostok 1', 'Sputnik', 'Luna 1', 'Mercury 3']
+            answers: ['Vostok 1', 'Sputnik', 'Luna 1', 'Mercury 3'],
+            answered: false,
         }, 
         400: {
             value: 400,
             question: 'This astronomer challenged the understanding of science and theologians when he published "On the Revolutions of the Heavenly Spheres".',
-            answers: ['Copernicus', 'Galileo', 'Euclid', 'Kepler']
+            answers: ['Copernicus', 'Galileo', 'Euclid', 'Kepler'],
+            answered: false,
         }, 
         500: {
             value: 500,
             question: 'This refers to the time period between two successive occurenes of a specific type of alignment between a planet, or one of its moons, with the Sun and the Earth.',
-            answers: ['Synodic Period', 'Conjunction', 'Sidereal Period', 'Aphelion']
+            answers: ['Synodic Period', 'Conjunction', 'Sidereal Period', 'Aphelion'],
+            answered: false,
         }
     }
 };
@@ -216,12 +246,17 @@ const answerQuestion = (e) => {
     qCard.style.display = 'none';
     category = e.currentTarget.getAttribute('category');
     value = e.currentTarget.getAttribute('value');
-    if (e.currentTarget.innerText === qAndA[category][value].answers[0]) {
+    console.log(qAndA[category][value].answered);
+    if (qAndA[category][value].answered === true) {
+        alert('You have already answered this question, please choose another.');
+    } else if (e.currentTarget.innerText === qAndA[category][value].answers[0]) {
         alert('Correct!');
         playerScore += qAndA[category][value].value;
+        qAndA[category][value].answered = true;
     } else {
         alert('Sorry, we were looking for ' + qAndA[category][value].answers[0])
         playerScore -= qAndA[category][value].value;
+        qAndA[category][value].answered = true;
     }
     
     updateScore();
